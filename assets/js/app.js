6a7a5d3a4af4bf7fd4414bce4231d5bd58d99fdf
kant146 <krishnakantkumar146@gmail.com>
2021-05-17 03:54:14 +0530

    kant checked

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 
 let header = $(`
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-<a class="navbar-brand" href="index.html">John Doe </a>
+<a class="navbar-brand" href="index.html">KRISHNAKANT </a>
 <div class="hamburger_wrapper navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
 
   <div id="js-hamburger" class="hamburger">
